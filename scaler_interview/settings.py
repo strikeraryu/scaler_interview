970244c07189b207b90b8c7c6feb2f5da00e67f1
strikeraryu <striker.aryu56@gmail.com>
2022-03-14 19:56:05 +0530

    changes

--- scaler_interview/settings.py
+++ scaler_interview/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z2qljp_xy#_!1)a@(yl4s36d77b)0a8pr=*rqt7@c7&730yj=='
+SECRET_KEY = '' # secret key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -135,8 +135,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'amata.family59@gmail.com'
-EMAIL_HOST_PASSWORD = 'aryu@60018'
+EMAIL_HOST_USER = '' # email id
+EMAIL_HOST_PASSWORD = '' # password
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
